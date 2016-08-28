0b2b669de553b145cbda70143f6ff8a808f2272c
MichaelCDormann <michael.c.dormann@gmail.com>
2016-08-27 23:30:19 -0400

    Some analysis ability

--- LexicalAnalyzer.py
+++ LexicalAnalyzer.py
@@ -1,16 +1,85 @@
+import re
+import sys
 
 tokens = {}
 tokens_temp = {"keyword": ("else", "if", "int", "return", "void", "while", "float"),
-               "symbol": ("+", "-", "*", "/", "<", "<=", ">", ">=", "==", "!=", "=", ";", ",", "(", ")", "[", "]", "{", "}", "/*", "*/")}
+               "symbol": ("+", "-", "*", "/", "//", "<", "<=", ">", ">=", "==", "!=", "=", ";", ",", "(", ")", "[", "]", "{", "}", "/*", "*/")}
+
 num = [i for i in range(0,9)]
 id = [chr(i) for i in range(65, 122) if not (90 < i < 97)]
 
 
 def reverse_token_dict():
-    global tokens_temp, tokens
+    global tokens_temp, tokens, token_keys
     for key, values in tokens_temp.iteritems():
         for value in values:
            tokens[value] = key
 
 reverse_token_dict()
 
+
+class FileAccessManager:
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def open(self):
+        return open(self.filename, 'r')
+
+
+class Printer:
+
+    def __init__(self, text):
+        self.text = text
+
+    def add(self, text):
+        self.text += text
+
+    def addLine(self, text):
+        self.text = self.text + "\n" + text
+
+    def prnt(self):
+        print self.text
+        self.text = ""
+
+
+class LexicalAnalyzer:
+
+    comment_counter = 0
+    scope_counter = 0
+    current_string = ""
+
+    def __init__(self, file_access_wrapper):
+        self.file = file_access_wrapper
+        self.printer = Printer("")
+
+    def analyze(self):
+        with self.file.open() as f:
+            line = f.readline()
+            self.printer.addLine("INPUT: " + line)
+
+            for character in line:
+                result = self.parse(character)
+                if result is not None:
+                    self.printer.addLine(result)
+
+        return self.printer
+
+    @staticmethod
+    def check_substring_in_list(substr, lst):
+        for value in lst:
+            if substr in value and substr != value:
+                return True
+        return False
+
+    @classmethod
+    def parse(cls, character):
+        cls.current_string += character
+
+        if cls.current_string in tokens and not cls.check_substring_in_list(cls.current_string, tokens.keys()):
+            return "" + tokens[cls.current_string] + ": " + cls.current_string
+
+
+#fl = FileAccessManager(sys.argv[1])
+#analyzer = LexicalAnalyzer(fl)
+#analyzer.analyze()
--- tests/test_LexicalAnalyzer.py
+++ tests/test_LexicalAnalyzer.py
@@ -1,7 +1,26 @@
 from LexicalAnalyzer import *
+import io
 
 def test_reverse_token_dict():
     reversed_dict = {"else": "keyword", "if": "keyword", "int": "keyword", "return": "keyword", "void": "keyword", "while": "keyword", "float": "keyword",
-               "+": "symbol", "-": "symbol", "*": "symbol", "/": "symbol", "<": "symbol", "<=": "symbol", ">": "symbol", ">=": "symbol", "==": "symbol", "!=": "symbol", "=": "symbol", ";": "symbol", ",": "symbol", "(": "symbol", ")": "symbol", "[": "symbol", "]": "symbol", "{": "symbol", "}": "symbol", "/*": "symbol", "*/": "symbol"}
+               "+": "symbol", "-": "symbol", "*": "symbol", "/": "symbol", "//": "symbol", "<": "symbol", "<=": "symbol", ">": "symbol", ">=": "symbol", "==": "symbol", "!=": "symbol", "=": "symbol", ";": "symbol", ",": "symbol", "(": "symbol", ")": "symbol", "[": "symbol", "]": "symbol", "{": "symbol", "}": "symbol", "/*": "symbol", "*/": "symbol"}
     reverse_token_dict()
     assert tokens == reversed_dict
+
+def test_parse():
+    output = "\nINPUT: int\nkeyword: int"
+
+    fake_file = FakeFile("int")
+    analyzer = LexicalAnalyzer(fake_file)
+    result = analyzer.analyze()
+
+    assert result.text == output
+
+
+class FakeFile:
+
+    def __init__(self, text):
+        self.text = text
+
+    def open(self):
+        return io.StringIO(self.text)
