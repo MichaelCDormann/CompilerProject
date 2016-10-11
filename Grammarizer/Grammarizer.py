5a5f6eb032d9f19e2e4435a07255639dcf359334
MichaelCDormann <michael.c.dormann@gmail.com>
2016-10-10 22:03:01 -0400

    First calc complete

--- Grammarizer/Grammarizer.py
+++ Grammarizer/Grammarizer.py
@@ -1,8 +1,8 @@
 import collections
 
-terminals = ["id", ";", "[", "]", "num", "int", "void", "float", "(", ")", ",", "{", "}", "if", "else", "while",
-             "return", "=", "<=", "<", ">", ">=", "==", "!=", "+", "-", "*", "/"]
-grammar = collections.OrderedDict([
+#terminals = ["id", ";", "[", "]", "num", "int", "void", "float", "(", ")", ",", "{", "}", "if", "else", "while",
+#             "return", "=", "<=", "<", ">", ">=", "==", "!=", "+", "-", "*", "/"]
+"""grammar = collections.OrderedDict([
 	("program",              ["declaration-list"]),
 	("declaration-list",     ["declaration-list declaration", "declaration"]),
 	("declaration",          ["var-declaration", "fun-declaration"]),
@@ -32,10 +32,21 @@ grammar = collections.OrderedDict([
 	("call",                 ["id ( args )"]),
 	("args",                 ["arg-list", "empty"]),
 	("arg-list",             ["arg-list , expression", "expression"])
+])"""
+
+
+terminals = ["a", "b", "c", "e", "f", "g", "empty"]
+grammar = collections.OrderedDict([
+	("S",   ["A B C D E"]),
+	("A",   ["a b A", "a b", "c", "empty"]),
+	("B",   ["C a", "D g", "empty"]),
+	("C",   ["b"]),
+	("D",   ["e", "empty"]),
+	("E",   ["f"])
 ])
 
-first = {}
-follow = {}
+first = collections.OrderedDict({})
+follow = collections.OrderedDict({})
 
 def print_grammar():
 	for rule, expression in grammar.iteritems():
@@ -44,5 +55,36 @@ def print_grammar():
 			expr_string = expr_string + expression[i] + (" | " if (len(expression) > 1 and len(expression) - 1 != i) else "")
 		print "{0:20} ->   {1}".format(rule, expr_string)
 
-def left_factor():
+def calc_first():
+	for rule, expressions in grammar.iteritems():
+		first_set = set()
+		for expr in expressions:
+			lexums = expr.split(" ")
+			first_set = first_set.union(get_first(lexums[0]))
+			index = 1
+			while "empty" in first_set and index < len(lexums):
+				first_set = first_set.difference(set(["empty"]))
+				first_set = first_set.union(get_first(lexums[index]))
+				index = index + 1
 
+		first[rule] = first_set
+
+def get_first(expr):
+	if expr in terminals:
+		return set([expr])
+	else:
+		firsts = set()
+		expressions = grammar[expr]
+		for expr in expressions:
+			lexums = expr.split(" ")
+			firsts = firsts.union(get_first(lexums[0]))
+			index = 1
+			while "empty" in firsts and index < len(lexums):
+				firsts = firsts.difference(set(["empty"]))
+				firsts = firsts.union(get_first(lexums[index]))
+				index = index + 1
+
+		return firsts
+
+calc_first()
+print first
