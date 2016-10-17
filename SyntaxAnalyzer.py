6defe58992e76accd00ffb6619d6199bc14da9b1
MichaelCDormann <michael.c.dormann@gmail.com>
2016-10-16 22:57:13 -0400

    Working well so far

--- SyntaxAnalyzer.py
+++ SyntaxAnalyzer.py
@@ -7,7 +7,7 @@ class SymbolTable(object):
 
 	@classmethod
 	def AddItem(cls, name, item):
-		if len(cls.dict_list) == 0:
+		if len(cls.dict_list) <= cls.depth:
 			cls.dict_list.append({})
 		cls.dict_list[cls.depth][name] = item
 
@@ -30,7 +30,7 @@ class SymbolTable(object):
 		elif depth < 0:
 			return None
 
-		if cls.dict_list[depth][name] is not None:
+		if name in cls.dict_list[depth].keys():
 			return cls.dict_list[depth][name]
 		else:
 			return cls.GetItem(name, depth-1)
@@ -44,9 +44,9 @@ class Var(object):
 
 class Func(object):
 
-	def __init__(self, ret_type, depth=None, params=[]):
+	def __init__(self, ret_type, params=[]):
 		self.ret_type = ret_type
-		self.depth = depth
+		#self.depth = depth
 		self.params = params
 		self.param_count = len(params)
 
@@ -74,7 +74,7 @@ class SyntaxAnalyzer(object):
 			else:
 				self.curString = self.curString + self.curToken + " "
 
-		if self.getParams == True:
+		if self.getParams:
 			if self.curToken in ["id", "num", "float_num"]:
 				self.paramString = self.paramString + self.curLexum + " "
 			else:
@@ -85,7 +85,7 @@ class SyntaxAnalyzer(object):
 		if self.index > len(self.token_lexum_list)-1:
 			self.curToken = "$"
 			return
-			#raise IndexError("Index out of range - no more tokens to process")
+			# raise IndexError("Index out of range - no more tokens to process")
 
 		self.curToken = self.token_lexum_list[self.index][0]
 		self.curLexum = self.token_lexum_list[self.index][1]
@@ -104,7 +104,7 @@ class SyntaxAnalyzer(object):
 		elif self.getParams == True:
 			tokens = self.curString.split(" ")
 			params = self.paramString.split(",")
-			SymbolTable.AddItem(tokens[1], Func(tokens[0], None, params))
+			SymbolTable.AddItem(tokens[1], Func(tokens[0], params))
 			self.curString = ""
 			self.paramString = ""
 			self.getParams = False
@@ -129,30 +129,30 @@ class SyntaxAnalyzer(object):
 		self.declist_prime()
 
 	def declist_prime(self):
-		if self.curToken in set(["int", "void", "float"]):
+		if self.curToken in ["int", "void", "float"]:
 			self.declaration()
 			self.declist_prime()
-		elif self.curToken in set(["$"]):
+		elif self.curToken == "$":
 			pass
 		else:
 			raise RejectException("Next token '" + self.curToken + "' not in first(declaration) or follow(declist_prime)")
 
 	def declaration(self):
-		if self.curToken in set(["int", "void", "float"]):
+		if self.curToken in ["int", "void", "float"]:
 			self.Accept()
 			self.dec_lf()
 		else:
 			raise RejectException("Next token was '" + self.curToken + " expected 'int', 'void', or 'float'")
 
 	def dec_lf(self):
-		if self.curToken in set(["id"]):
+		if self.curToken == "id":
 			self.Accept()
 			self.dec_lf_lf()
 		else:
 			raise RejectException("Next token was '" + self.curToken + "' expected 'id'")
 
 	def dec_lf_lf(self):
-		if self.curToken in set([";", "["]):
+		if self.curToken in [";", "["]:
 			if self.curToken == "[":
 				self.Accept()
 				if self.curToken == "num":
@@ -173,12 +173,12 @@ class SyntaxAnalyzer(object):
 				raise RejectException("Next token was '" + self.curToken + "' was expecting '[' or ';'")
 			self.InsertST()
 
-		elif self.curToken in set(["("]):
+		elif self.curToken == "(":
 			self.Accept()
 			self.getParams = True
 			self.params()
 
-			if self.curToken in set([")"]):
+			if self.curToken == ")":
 				self.InsertST()
 				self.Accept()
 				self.compoundstmt()
@@ -189,7 +189,7 @@ class SyntaxAnalyzer(object):
 			raise RejectException("Next token was '" + self.curToken + "' was expecting ';', '[', or '('")
 
 	def params(self):
-		if self.curToken in set(["int", "float"]):
+		if self.curToken in ["int", "float"]:
 			self.Accept()
 			if self.curToken == "id":
 				self.Accept()
@@ -197,20 +197,42 @@ class SyntaxAnalyzer(object):
 				self.paramlist_prime()
 			else:
 				raise RejectException("Next token was '" + self.curToken + "' was expecting 'id'")
-		elif self.curSymbol == "void":
+		elif self.curToken == "void":
 			self.Accept()
 			self.params_lf()
 		else:
 			raise RejectException("Next token was '" + self.curToken + "' was expecting 'int', 'void' or 'float'")
 
 	def params_lf(self):
-		pass
+		if self.curToken == "id":
+			self.Accept()
+			self.param_lf()
+			self.paramlist_prime()
+		elif self.curToken == ")":
+			pass
+		else:
+			raise RejectException("Next token was '" + self.curToken + "' was expecting 'id' or ')'")
 
 	def paramlist_prime(self):
-		pass
+		if self.curToken == ",":
+			self.Accept()
+			self.param()
+			self.paramlist_prime()
+		elif self.curToken == ")":
+			pass
+		else:
+			raise RejectException("Next token was '" + self.curToken + "' was expecting ',' or ')'")
 
 	def param(self):
-		pass
+		if self.curToken in ["int", "void", "float"]:
+			self.Accept()
+			if self.curToken == "id":
+				self.Accept()
+				self.param_lf()
+			else:
+				raise RejectException("Next token was '" + self.curToken + "' was expecting 'id'")
+		else:
+			raise RejectException("Next token was '" + self.curToken + "' was expecting 'int', 'void' or 'float'")
 
 	def param_lf(self):
 		if self.curToken == "[":
@@ -219,16 +241,104 @@ class SyntaxAnalyzer(object):
 				self.Accept()
 			else:
 				raise RejectException("Next token was '" + self.curToken + "' was expecting ']'")
-		elif self.curToken in set([")", ","]):  # [")", "[", ","]
+		elif self.curToken in [")", ","]:  # [")", "[", ","]
 			pass
 		else:
 			raise RejectException("Next token was '" + self.curToken + "' was expecting ')'")
 
 	def compoundstmt(self):
-		pass
+		if self.curToken == "{":
+			self.Accept()
+			self.localdeclarations()
+			self.statementlist()
+			if self.curToken == "}":
+				self.Accept()
+			else:
+				raise RejectException("Next token was '" + self.curToken + "' was expecting '}'")
+		else:
+			raise RejectException("Next token was '" + self.curToken + "' was expecting '{'")
 
+	def localdeclarations(self):
+		if self.curToken in ["int", "void", "float"]:
+			self.Accept()
+			self.dec_lf()
+			self.localdeclarations()
+		elif self.curToken in ["return", "(", "id", "while", "num", "float_num", ";", "}", "{", "if"]:
+			pass
+		else:
+			raise RejectException(
+				"Next token was '" + self.curToken + "' was expecting 'int', 'void', 'float', 'return', '(', 'id', "
+				                                     "'while', 'num', 'float_num', ';', '}', '{', or 'if'")
 
+	def statementlist(self):
+		if self.curToken in ["return", "(", "while", "num", "float_num", "{", ";", "id", "if"]:
+			self.statement()
+			self.statementlist()
+		elif self.curToken == "}":
+			pass
+		else:
+			raise RejectException(
+				"Next token was '" + self.curToken + "' was expecting 'return', '(', 'while', 'num', 'float_num', '{', "
+				                                     "';', 'id', or 'if'")
 
+	def statement(self):
+		if self.curToken in ["(", ";", "num", "id", "float_num"]:
+			self.expressionstmt()
+		elif self.curToken == "{":
+			self.compoundstmt()
+		elif self.curToken == "if":
+			self.selectionstmt()
+		elif self.curToken == "while":
+			self.iterationstmt()
+		elif self.curToken == "return":
+			self.returnstmt()
+		else:
+			raise RejectException(
+				"Next token was '" + self.curToken + "' was expecting 'return', '(', 'while', 'num', 'float_num', '{', "
+				                                     "';', 'id', or 'if'")
+
+	def expressionstmt(self):
+		if self.curToken in ["(", "num", "id", "float_num"]:
+			self.expression()
+			if self.curToken == ";":
+				self.Accept()
+			else:
+				raise RejectException("Next token was '" + self.curToken + "' was expecting ';'")
+		elif self.curToken == ";":
+			self.Accept()
+		else:
+			raise RejectException("Next token was '" + self.curToken + "' was expecting '(', 'num', 'id', 'float_num', or ';'")
+
+	def selectionstmt(self):
+		if self.curToken == "if":
+			self.Accept()
+			if self.curToken == "(":
+				self.Accept()
+				self.expression()
+				if self.curToken == ")":
+					self.Accept()
+					self.statement()
+					self.selstmt_lf()
+				else:
+					raise RejectException("Next token was '" + self.curToken + "' was expecting ')'")
+			else:
+				raise RejectException("Next token was '" + self.curToken + "' was expecting '('")
+		else:
+			raise RejectException("Next token was '" + self.curToken + "' was expecting 'if'")
+
+	def selstmt_lf(self):
+		if self.curToken == "else":
+			self.Accept()
+			self.statement()
+		elif self.curToken in ["return", "(", "while", "num", "float_num", "{", ";", "}", "id", "if"]:
+			pass
+		else:
+			raise RejectException(
+				"Next token was '" + self.curToken + "' was expecting 'return', '(', 'else', 'while', 'num', 'float_num', "
+				                                     "'{', ';', '}', 'id', and 'if'")
+
+	def expression(self):
+		self.Accept()
 
 class RejectException(Exception):
 	def __init__(self,*args,**kwargs):
